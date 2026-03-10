6521bfcfa4e7703ea963362a7932c83cbe506e81
prakharyadav-9 <py@example.com>
2026-03-10 20:44:18 +0530

    feat: used local model with ollama

---
+++ README.md
@@ -0,0 +1,14 @@
+# Project Description
+This is a learning project, to add skills like LangChain, LangGraph, etc.
+
+## Cloud model
+- i am using Google Gemini as LLM provider, in Free Tier.
+* Mostly `model="gemini-2.5-flash"`
+Command to add Google Gemini in LangChain: ``uv add langchain-google-genai``
+
+## How to run Local model.
+I am using ollama, to run a model. A light weight model as there are hardware limitation:
+- CPU: i5 10th Gen
+- RAM (DDR4): 16GB
+- GPU: AMD Radeon 610 Series
+- Dedicated GPU memory: 2GB
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
@@ -8,22 +9,19 @@ load_dotenv()
 def main():
     print("Hello from langchain-course-py!")
     information = """
-Avul Pakir Jainulabdeen Abdul Kalam ( born on 15 October 1931 – 27 July 2015) was an Indian aerospace scientist and statesman who served as the president of India from 2002 to 2007.
-Born and raised in a Muslim family in Rameswaram, Tamil Nadu, Kalam studied physics and aerospace engineering. He was known as the "Missile Man of India" for his work on the development of ballistic missile and launch vehicle technology. 
-Kalam was elected as the president of India in 2002.
+Prakhar
     """
     """// this information we are going to propoage to LLM"""
 
     summary_template = """
-    Given the information {information} about a person I want you to create:
-    1. a short summary of the information
-    2. two interesting facts about them
+    Given the name of a peron {information}, I want you to greet the person, with his name.
     """
     summary_prompt_template = PromptTemplate(
         input_variables = ["information"], template = summary_template
     )
-    llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0) # an object which will interact with model
+    # llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0) # an object which will interact with model
     # i had to use a basic model "gemini-2.5-flash" to even generate an ouput
+    llm = ChatOllama(temperature = 0.4, model = "qwen2.5")
     chain = summary_prompt_template | llm
     response = chain.invoke(input={"information": information})
 
