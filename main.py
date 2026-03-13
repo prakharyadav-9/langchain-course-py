4c25500d9d259e0e1e64ec32d1dd9de329ec77bd
prakharyadav-9 <py@example.com>
2026-03-14 03:11:53 +0530

    chore: integrated structed output with some examples images.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,6 +11,23 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from datetime import datetime
 from tavily import TavilyClient
 from langchain_tavily import TavilySearch
+
+
+class Source(BaseModel):
+    """
+    Schema for a source used by the agent
+    """
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """
+    Schema for agent response with answer and sources
+    """
+    answer: str = Field(description="the agent's answer to the query")
+    # default_factory corresponds to Source class and if there are no sources it will be empty
+    sources: List[Source] = Field(default_factory=list, description="list of sources used to generate the answer")
+    #this class will make sure our agent responses are structured like this class which can be downstreamed to an application further, for many purposes
+
 
 tavily = TavilyClient() # here it will look for env variable TAVILY_API_KEY
 tavily_search = TavilySearch() # this is a wrapper that tavily team has given for search check https://docs.tavily.com/welcome for more
@@ -39,7 +59,8 @@ tools = [search_internal
          , internal_tool_to_get_current_date
          , tavily_search
          ]
-agent = create_agent(model = llm, tools= tools)
+agent = create_agent(model = llm, tools= tools, response_format=AgentResponse)
+#after adding response_format a new key will be present in the response of the agent as `structed-response` whose type is AgentResponse as specified, which will have both the field specified, answer and sources
 
 def main():
     print("Hello from langchain-course-py!")
@@ -48,7 +69,8 @@ def main():
     # agent runnable 
     result = agent.invoke({"messages":[HumanMessage(content=content)]})
     print("Yup, the agent returned and answer...")
-    print(result)
+    print(result["structured_response"].answer)
+    print(result["structured_response"].sources)
 
 
 if __name__ == "__main__":
